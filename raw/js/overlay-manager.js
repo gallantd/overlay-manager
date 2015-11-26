3b757752cebd8b4d31384e563d5b4a759c1e3bd8
David Gallant <david.gallant@viacomcontractor.com>
2015-11-26 13:13:45 -0400

    working

--- raw/js/overlay-manager.js
+++ raw/js/overlay-manager.js
@@ -38,11 +38,23 @@ OverlayManager.prototype.FRAME_ID = 'OverlayFrame';
 OverlayManager.prototype.OVERLAY_ID = 'OverlayModal';
 
 /**
+ * [adFreeCheck description]
+ * @return {[type]} [description]
+ */
+OverlayManager.prototype.adFreeCheck = function adFreeCheck() {
+	if (!this.adfree || this.adfree === this.options.onAdfree){
+		return true;
+	}
+	return false;
+};
+
+/**
  * all function calls needed for overlay
  */
 OverlayManager.prototype.batting = function batting(){
 	var iShow;
 	var iMatch = this.overlayCheck();
+
 	if (typeof iMatch !== 'object') {
 		this.logging('no match provided', 'yarn');
 		return false;
@@ -58,80 +70,32 @@ OverlayManager.prototype.batting = function batting(){
 };
 
 /**
- * defaults for overlay
- * @type {Object}
+ * Check provided url to see if it is an image
+ * @return {Boolean} [return true if image false if not]
  */
-OverlayManager.prototype.defaults = {
-	cookieTime: 1,
-	cookieValue: 'true',
-	cookieName: 'OverlayCookie',
-	width: '100%',
-	height: '100%',
-	backgroundColor: '0,0,0',
-	opacity: '0.8',
-	onAdfree: false,
-	deferCookie: false,
-	currentUrl: window.location.href
-};
+OverlayManager.prototype.checkContents = function checkContents(){
+	var iImage = this.options.customIncludeUrl.split('.');
+	iImage = iImage[iImage.length - 1].toLowerCase();
 
-/**
- * Check includes and excludes to see if the overlay should live on the page
- */
-OverlayManager.prototype.overlayCheck = function overlayCheck() {
-	var i, iMatch;
-	var promos = this.zones;
-	var count = promos.length;
-
-	for (i = 0; i < count; i++) {
-		// Early escape when no parameters are set.
-		if (!promos[i].parameters) {
-			iMatch = i;
-			continue;
-		}
-
-
-		/*
-		NOTE: NEED TO CHECK FOR THE BROWSER HERE SO WE DON'T GET A FALSE
-		POSITIVE AND THEN HAVE TO RECHECK FOR A BETTER MATCH.
-		 */
-
-		if(promos[i].parameters.includes) {
-			// If include match we need not process any other.
-			if (this.matchCheck(promos[i].parameters.includes.split(','))) {
-				iMatch = i;
-				break;
-			}
-			continue;
-		}
-
-		if(promos[i].parameters.excludes) {
-			// If excludes match we need to break out of this check.
-			if (this.matchCheck(promos[i].parameters.excludes.split(','))) {
-				continue;
-			}
-		}
-
-		iMatch = i;
+	if (iImage === 'jpg' || iImage === 'png' || iImage === 'gif') {
+		return true;
 	}
 
-	if (iMatch !== undefined) {
-	//	this.logging('Matching page overlay');
-		return promos[iMatch];
-	}
 	return false;
 };
 
-OverlayManager.prototype.matchCheck = function matchCheck(value) {
-	var i, valueLength, url;
-	if (!value) {
-		return false;
-	}
+/**
+ * check to see the status of the current cookie
+ * @return {boolean}
+ */
+OverlayManager.prototype.checkCookie = function checkCookie(cookieMatch) {
+	if (!cookieMatch) {return false;}
+	var i,
+		current = document.cookie.split(';'),
+		count = current.length;
 
-	valueLength = value.length;
-	url = this.defaults.currentUrl;
-
-	for (i = 0; i < valueLength; i++) {
-		if (url.match(value[i])) {
+	for (i = 0; i < count; i++) {
+		if (current[i] !== '' && current[i].indexOf(cookieMatch) > -1) {
 			return true;
 		}
 	}
@@ -140,24 +104,66 @@ OverlayManager.prototype.matchCheck = function matchCheck(value) {
 };
 
 /**
- * Set options for the overlay
- * @param {object} options [contains all the zone information]
+ * [checkDevice description]
+ * @param  {[type]} device [description]
+ * @return {[type]}        [description]
  */
-OverlayManager.prototype.setOptions = function setOptions(data) {
-	if (!data) {
-		this.logging('No data options provided', 'error');
-		return false;
+OverlayManager.prototype.checkDevice = function checkDevice(device) {
+	var i =0,
+	count = device.length;
+	//document.cookie = 'current_device=site-tablet.com;100000000;path=/';
+
+
+	for (;i < count; i++) {
+		if (this.checkCookie(device[i])) {
+			this.logging('Device cookie match', 'warn');
+			return true;
+		} else if (this.deviceSize(device[i])) {
+			this.logging('Device size match', 'warn');
+			return true;
+		}
 	}
 
-	this.options = $.extend(this.options, this.defaults, data.parameters);
-	this.options.customIncludeUrl = data.customIncludeUrl;
+	return false;
 };
 
-OverlayManager.prototype.adFreeCheck = function adFreeCheck() {
-	if (!this.adfree || this.adfree === this.options.onAdfree){
-		return true;
+/**
+ * Create frame container for overlay, if frame is successfully created setup listeners
+ */
+OverlayManager.prototype.createFrame = function createFrame() {
+	var content = this.checkContents();
+	var height = parseInt(this.options.height, 10);
+	var units = this.options.height.replace(height, '');
+	var frame = content? document.createElement('img') : document.createElement('iframe');
+
+	frame.id = this.FRAME_ID;
+	frame.style.width = this.options.width;
+	frame.style.height = this.options.height;
+	frame.style.backgroundColor = 'transparent';
+	frame.style.position = 'relative';
+	frame.style.border = 'none';
+	frame.style.zIndex = 10;
+	frame.style.verticalAlign = 'center';
+	frame.allowTransparency = 'true';
+	frame.frameBorder = 0;
+	frame.scrolling = 'no';
+	frame.src = this.options.customIncludeUrl;
+
+	if ('%' === units) {
+		frame.style.top = ((100 - height) / 2) + units;
+	} else {
+		frame.style.top = '50%';
+		frame.style.marginTop = '-' + Math.floor(height / 2) + units;
 	}
-	return false;
+
+	if (content && this.options.link) {
+		this.$body.find('#' + this.OVERLAY_ID).append(
+			$('<a>').attr('href',this.options.link)
+			.append(frame)
+			).show();
+	} else {
+		this.$body.find('#' + this.OVERLAY_ID).append(frame).show();
+	}
 };
 
 /**
@@ -170,7 +176,7 @@ OverlayManager.prototype.cookieStatus = function cookieStatus() {
 		return true;
 	}
 
-	if (!this.checkCookie()) {
+	if (!this.checkCookie(this.options.cookieName)) {
 		return this.setCookie();
 	} else {
 		this.logging(this.options.cookieName + 'cookie has already been set', 'warn');
@@ -178,40 +184,28 @@ OverlayManager.prototype.cookieStatus = function cookieStatus() {
 };
 
 /**
- * check to see the status of the current cookie
- * @return {boolean}
+ * Create the close button to close the overlay container
  */
-OverlayManager.prototype.checkCookie = function checkCookie() {
-	var i,
-		current = document.cookie.split(';'),
-		count = current.length;
-
-	for (i = 0; i < count; i++) {
-
-		if (current[i] !== '' && current[i].indexOf(this.options.cookieName) > -1) {
-			return true;
-		}
-	}
-
-	return false;
-};
-
-/**
- * set a cookie when needed
- */
-OverlayManager.prototype.setCookie = function setCookie() {
-	var expires,
-		cookie,
-		thisDate = new Date();
-
-	thisDate.setTime(thisDate.getTime() + (this.options.cookieTime * 24 * 60 * 60 * 1000));
-	expires = 'expires=' + thisDate.toUTCString();
-	cookie = this.options.cookieName + '=' + this.options.cookieValue + ';' + expires + ';' + 'path=/';
-
-	if (document.cookie = cookie) {
-		return true;
-	}
-	return false;
+OverlayManager.prototype.createClose = function createClose() {
+	var close = {
+		'width':'35px',
+		'height':'35px',
+		'color':'white',
+		'border':'2px solid white',
+		'background':'black',
+		'border-radius':'25px',
+		'text-decoration':'none',
+		'display': 'block',
+		'font': '30px/1 Arial',
+		'font-weight': 'Bold',
+		'position': 'absolute',
+		'right': '1%',
+		'top': '2%',
+		'zIndex': '99999'
+	};
+	this.$body.find('#' + this.OVERLAY_ID).append(
+		$('<a>').attr('href','#').text('x').css(close).on('click',$.proxy(this.remove, this))
+	);
 };
 
 /**
@@ -240,102 +234,55 @@ OverlayManager.prototype.createMask = function createMask() {
 };
 
 /**
- * Create the close button to close the overlay container
+ * defaults for overlay
+ * @type {Object}
  */
-OverlayManager.prototype.createClose = function createClose() {
-	var close = {
-		'width':'35px',
-		'height':'35px',
-		'color':'white',
-		'background':'black',
-		'border-radius':'25px',
-		'text-decoration':'none',
-		'display': 'block',
-		'font': '30px/1 Arial',
-		'font-weight': 'Bold',
-		'position': 'absolute',
-		'right': '1%',
-		'top': '2%',
-		'zIndex': '99999'
-	};
-	this.$body.find('#' + this.OVERLAY_ID).append(
-		$('<a>').attr('href','#').text('x').css(close).on('click',$.proxy(this.remove, this))
-	);
+OverlayManager.prototype.defaults = {
+	cookieTime: 1,
+	cookieValue: 'true',
+	cookieName: 'OverlayCookie',
+	width: '100%',
+	height: '100%',
+	backgroundColor: '0,0,0',
+	opacity: '0.8',
+	onAdfree: false,
+	deferCookie: false,
+	currentUrl: window.location.href
 };
 
 /**
- * Check provided url to see if it is an image
- * @return {Boolean} [return true if image false if not]
+ * [sizeDevice description]
+ * @return {[type]} [description]
  */
-OverlayManager.prototype.checkContents = function checkContents(){
-	var iImage = this.options.customIncludeUrl.split('.');
-	iImage = iImage[iImage.length - 1].toLowerCase();
+OverlayManager.prototype.deviceSize = function deviceSize(current) {
+	var deviceMax , deviceMin;
+	var device = 'desktop';
+	var deviceW = this.$body.width();
 
-	if (iImage === 'jpg' || iImage === 'png' || iImage === 'gif') {
+	if (window.matchMedia('(orientation: landscape)').matches) {
+
+		//iPad size
+		deviceMax = 1024;
+		deviceMin = 668;
+
+	} else {
+		// iPhone 6
+		deviceMax = 768;
+		deviceMin = 375;
+
+	}
+
+	if (deviceW <= deviceMin) {
+		device = 'mobile';
+	} else if (deviceW <= deviceMax) {
+		device = 'tablet';
+	}
+
+	if (current === device) {
 		return true;
 	}
 
 	return false;
-};
-
-/**
- * Create frame container for overlay, if frame is successfully created setup listeners
- */
-OverlayManager.prototype.createFrame = function createFrame() {
-	var content = this.checkContents();
-	var frame = content? document.createElement('img') : document.createElement('iframe');
-
-	frame.id = this.FRAME_ID;
-	frame.style.width = this.options.width;
-	frame.style.height = this.options.height;
-	frame.style.backgroundColor = 'transparent';
-	frame.style.position = 'relative';
-	frame.style.border = 'none';
-	frame.style.zIndex = 10;
-	frame.style.verticalAlign = 'center';
-	frame.allowTransparency = 'true';
-	frame.frameBorder = 0;
-	frame.scrolling = 'no';
-	frame.src = this.options.customIncludeUrl;
-	frame.style.top = '50%';
-	frame.style.marginTop = '-' + parseInt(this.options.height, 10) / 2 + 'px';
-
-	if (content && this.options.link) {
-		this.$body.find('#' + this.OVERLAY_ID).append(
-			$('<a>').attr('href',this.options.link)
-			.append(frame)
-			).show();
-	} else {
-		this.$body.find('#' + this.OVERLAY_ID).append(frame).show();
-	}
-};
-
-/**
- * setup listners for closing overlay
- */
-OverlayManager.prototype.setupListeners = function setupListeners() {
-	var success = true;
-
-	if (this.$body.hasClass('activeOverlay')) {
-		success = false;
-	}
-
-	this.$body.toggleClass('activeOverlay', true);
-	this.$window.bind('overlay.kill', $.proxy(this.remove, this));
-
-	return success;
- };
-
-/**
- * remove listeners and overlay
- */
-OverlayManager.prototype.remove = function remove() {
-	if (!this.$body.hasClass('activeOverlay')) {
-		return;
-	}
-
-	this.$body.toggleClass('activeOverlay', false).find('#' + this.OVERLAY_ID).remove();
-	this.$window.unbind('overlay.kill');
 };
 
 /**
@@ -361,4 +308,157 @@ OverlayManager.prototype.logging = function logging(message, type) {
 			console.log('OverlayManager Obj-> ', message);
 		}
 	}
+};
+
+OverlayManager.prototype.matchCheck = function matchCheck(value) {
+	var i, valueLength, url;
+	if (!value) {
+		return false;
+	}
+
+	valueLength = value.length;
+	url = this.defaults.currentUrl;
+
+	for (i = 0; i < valueLength; i++) {
+		if (url.match(value[i])) {
+			return true;
+		}
+	}
+
+	return false;
+};
+
+/**
+ * Check includes and excludes to see if the overlay should live on the page
+ */
+OverlayManager.prototype.overlayCheck = function overlayCheck() {
+	var i, iMatch, zone;
+	var promos = this.zones;
+	var count = promos.length;
+
+	for (i = 0; i < count; i++) {
+		// Early escape when no parameters are set.
+		if (!promos[i].parameters) {
+			iMatch = i;
+			continue;
+		}
+		zone = promos[i].parameters;
+
+		if (zone.device && !this.checkDevice(zone.device.split(','))) {
+			continue;
+		}
+
+		if (zone.includes) {
+			// If include match we need not process any other.
+			if (this.matchCheck(zone.includes.split(','))) {
+				iMatch = i;
+				break;
+			}
+			continue;
+		}
+
+		if (zone.excludes) {
+			// If excludes match we need to break out of this check.
+			if (this.matchCheck(zone.excludes.split(','))) {
+				continue;
+			}
+		}
+
+		iMatch = i;
+	}
+
+	if (iMatch !== undefined) {
+		this.logging('Matching page overlay', 'warn');
+		return promos[iMatch];
+	}
+	return false;
+};
+
+/**
+ * remove listeners and overlay
+ */
+OverlayManager.prototype.remove = function remove() {
+	if (!this.$body.hasClass('activeOverlay')) {
+		return;
+	}
+
+	this.$body.toggleClass('activeOverlay', false).find('#' + this.OVERLAY_ID).remove();
+	this.$window.unbind('overlay.kill');
+	this.logging('Closing overlay', 'warn');
+};
+
+/**
+ * [runLoop description]
+ * @param  {[type]} contents [description]
+ * @param  {[type]} check    [description]
+ * @return {[type]}          [description]
+ */
+OverlayManager.prototype.runLoop = function runLoop(contents, check) {
+	// Run through array and check for cookie value.
+	//
+	//
+	// take current size and checks includes.
+	contents.forEach(function(current){
+		if (check === current) {
+			return true;
+		}
+	});
+
+	return false;
+};
+
+/**
+ * set a cookie when needed
+ */
+OverlayManager.prototype.setCookie = function setCookie() {
+	var expires,
+		cookie,
+		thisDate = new Date();
+
+	thisDate.setTime(thisDate.getTime() + (this.options.cookieTime * 24 * 60 * 60 * 1000));
+	expires = 'expires=' + thisDate.toUTCString();
+	cookie = this.options.cookieName + '=' + this.options.cookieValue + ';' + expires + ';' + 'path=/';
+
+	if (document.cookie = cookie) {
+		return true;
+	}
+	return false;
+};
+
+/**
+ * Set options for the overlay
+ * @param {object} options [contains all the zone information]
+ */
+OverlayManager.prototype.setOptions = function setOptions(data) {
+	if (!data) {
+		this.logging('No data options provided', 'error');
+		return false;
+	}
+
+	if (data.parameters) {
+		this.options = $.extend(this.options, this.defaults, data.parameters);
+		this.options.customIncludeUrl = data.customIncludeUrl;
+
+	} else {
+		this.options = $.extend(this.options, this.defaults, data);
+	}
+
+	this.logging(this.options, 'warn');
+
+};
+
+/**
+ * setup listners for closing overlay
+ */
+OverlayManager.prototype.setupListeners = function setupListeners() {
+	var success = true;
+
+	if (this.$body.hasClass('activeOverlay')) {
+		success = false;
+	}
+
+	this.$body.toggleClass('activeOverlay', true);
+	this.$window.bind('overlay.kill', $.proxy(this.remove, this));
+
+	return success;
 };
